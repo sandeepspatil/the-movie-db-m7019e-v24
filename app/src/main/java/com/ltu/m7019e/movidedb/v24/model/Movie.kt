22ce9894285d9e5e33d7c13caac08cfcdd32b137
Sandeep Patil <sandeep.spatil@gmail.com>
2024-04-04 16:07:49 +0200

    Display hardcoded list of Movies in a Scrollable list using LazyColumn

---
+++ app/src/main/java/com/ltu/m7019e/movidedb/v24/model/Movie.kt
@@ -0,0 +1,10 @@
+package com.ltu.m7019e.movidedb.v24.model
+
+data class Movie(
+    var id: Long = 0L,
+    var title: String,
+    var posterPath: String,
+    var backdropPath: String,
+    var releaseDate: String,
+    var overview: String
+)
---
+++ app/src/main/java/com/ltu/m7019e/movidedb/v24/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.ltu.m7019e.movidedb.v24.utils
+
+object Constants {
+    const val POSTER_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val POSTER_IMAGE_WIDTH = "w92"
+    const val API_KEY = SECRETS.API_KEY
+}
